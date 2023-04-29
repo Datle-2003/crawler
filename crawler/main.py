e7a79c08a928144b90fc412dea8ece2bebbb7d7e
lethanhdat <92386974+Datle-2003@users.noreply.github.com>
2023-04-29 18:33:41 +0700

    Update main.py

--- crawler/main.py
+++ crawler/main.py
@@ -17,41 +17,41 @@ while True:
 
 
 # Run the second command
-while True:
-    # Run the command
-    status = os.system("scrapy crawl tgdd")
+# while True:
+#     # Run the command
+#     status = os.system("scrapy crawl tgdd")
 
-    # Check the exit status
-    if status == 0:
-        # The command succeeded, so break out of the loop
-        break
-    else:
-        # The command failed, so wait for a bit and try again
-        time.sleep(10)
-
-
-while True:
-    # Run the command
-    status = os.system("scrapy crawl dienmayxanh")
+#     # Check the exit status
+#     if status == 0:
+#         # The command succeeded, so break out of the loop
+#         break
+#     else:
+#         # The command failed, so wait for a bit and try again
+#         time.sleep(10)
 
 
-    # Check the exit status
-    if status == 0:
-        # The command succeeded, so break out of the loop
-        break
-    else:
-        # The command failed, so wait for a bit and try again
-        time.sleep(10)
+# while True:
+#     # Run the command
+#     status = os.system("scrapy crawl dienmayxanh")
 
-while True:
-    # Run the command
-    status = os.system("scrapy crawl hoanghamobile")
 
-    # Check the exit status
-    if status == 0:
-        # The command succeeded, so break out of the loop
-        break
-    else:
-        # The command failed, so wait for a bit and try again
-        time.sleep(10)
+#     # Check the exit status
+#     if status == 0:
+#         # The command succeeded, so break out of the loop
+#         break
+#     else:
+#         # The command failed, so wait for a bit and try again
+#         time.sleep(10)
+
+# while True:
+#     # Run the command
+#     status = os.system("scrapy crawl hoanghamobile")
+
+#     # Check the exit status
+#     if status == 0:
+#         # The command succeeded, so break out of the loop
+#         break
+#     else:
+#         # The command failed, so wait for a bit and try again
+#         time.sleep(10)
 
