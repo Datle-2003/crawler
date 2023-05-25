d0d8662ea174ddffe707bbd25319ff4c57374760
lethanhdat <92386974+Datle-2003@users.noreply.github.com>
2023-05-25 20:16:21 +0700

    Update main.py

--- crawler/main.py
+++ crawler/main.py
@@ -19,41 +19,82 @@ while True:
 
 
 # Run the second command
-# while True:
-#     # Run the command
-#     status = os.system("scrapy crawl tgdd")
+while True:
+    # Run the command
+    status = os.system("scrapy crawl tgdd")
 
-#     # Check the exit status
-#     if status == 0:
-#         # The command succeeded, so break out of the loop
-#         break
-#     else:
-#         # The command failed, so wait for a bit and try again
-#         time.sleep(10)
-
-
-# while True:
-#     # Run the command
-#     status = os.system("scrapy crawl dienmayxanh")
+    # Check the exit status
+    if status == 0:
+        # The command succeeded, so break out of the loop
+        break
+    else:
+        # The command failed, so wait for a bit and try again
+        time.sleep(10)
 
 
-#     # Check the exit status
-#     if status == 0:
-#         # The command succeeded, so break out of the loop
-#         break
-#     else:
-#         # The command failed, so wait for a bit and try again
-#         time.sleep(10)
+while True:
+    # Run the command
+    status = os.system("scrapy crawl dienmayxanh")
 
-# while True:
-#     # Run the command
-#     status = os.system("scrapy crawl hoanghamobile")
 
-#     # Check the exit status
-#     if status == 0:
-#         # The command succeeded, so break out of the loop
-#         break
-#     else:
-#         # The command failed, so wait for a bit and try again
-#         time.sleep(10)
+    # Check the exit status
+    if status == 0:
+        # The command succeeded, so break out of the loop
+        break
+    else:
+        # The command failed, so wait for a bit and try again
+        time.sleep(10)
+
+
+        
+        
+while True:
+    # Run the command
+    status = os.system("scrapy crawl hoanghamobile")
+
+    # Check the exit status
+    if status == 0:
+        # The command succeeded, so break out of the loop
+        break
+    else:
+        # The command failed, so wait for a bit and try again
+        time.sleep(10)
+
+
+while True:
+    # Run the command
+    status = os.system("scrapy crawl anphatpc")
+
+    # Check the exit status
+    if status == 0:
+        # The command succeeded, so break out of the loop
+        break
+    else:
+        # The command failed, so wait for a bit and try again
+        time.sleep(10)
+
+while True:
+    # Run the command
+    status = os.system("scrapy crawl hacom")
+
+    # Check the exit status
+    if status == 0:
+        # The command succeeded, so break out of the loop
+        break
+    else:
+        # The command failed, so wait for a bit and try again
+        time.sleep(10)
+
+
+while True:
+    # Run the command
+    status = os.system("scrapy crawl gearvn")
+
+    # Check the exit status
+    if status == 0:
+        # The command succeeded, so break out of the loop
+        break
+    else:
+        # The command failed, so wait for a bit and try again
+        time.sleep(10)
 
